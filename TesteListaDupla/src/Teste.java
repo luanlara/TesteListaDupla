0ec5e8c11489e399eeee77e0bdf6db8abf022dab
Luan Lara de Moraes <luanlaramoraes@gmail.com>
2022-03-11 12:21:38 -0300

    minha primeira alteração

---
+++ TesteListaDupla/src/Teste.java
@@ -0,0 +1,16 @@
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		Lista lista = new Lista();
+		
+		lista.inicio = null;
+		lista.inserir("Luan", 123);
+		lista.inserir("Julia", 456);
+		
+		lista.imprimir();		
+
+	}
+
+}
